197285bec53594c1178490e44c8b721516c495ca
johnathanthedev <johnathanguzman17@gmail.com>
2021-12-02 22:02:20 -0600

    Fixed token retrieval issue

--- src/components/auth/PrivateRoute.tsx
+++ src/components/auth/PrivateRoute.tsx
@@ -10,8 +10,7 @@ const PrivateRoute = ({
   path,
   ...rest
 }: PrivateRouteProps) => {
-  // const { isLoading, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
-  const { isLoading, isAuthenticated } = useAuth0();
+  const { isLoading, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
   let history = useHistory();
 
   useEffect(():any  => {
@@ -24,36 +23,17 @@ const PrivateRoute = ({
     recursiveFunc()
   }, [isAuthenticated, isLoading, history]);
 
-  // useEffect(() => {
-  //   const getUserMetadata = async () => {
-  //     const token = await getAccessTokenSilently()
-      // const auth0_user_obj = {
-      //   user,
-        // client_access_token: token 
-      // }
-
-      // console.log(auth0_user_obj)
-    // }
-    // getUserMetadata()
-  // }, [user, getAccessTokenSilently])
-
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_AUTH0_NETFLIX_CLONE_TOKEN_URL}`, {
-  //     method: "POST",
-  //     // mode: 'no-cors', 
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       "client_id":`${process.env.REACT_APP_AUTH0_NETFLIX_CLONE_API_CLIENT_ID}`,
-  //       "client_secret":`${process.env.REACT_APP_AUTH0_NETFLIX_CLONE_API_CLIENT_SECRET}`,
-  //       "audience":`${process.env.REACT_APP_AUTH0_NETFLIX_CLONE_AUDIENCE}`,
-  //       "grant_type":"client_credentials"
-  //     })
-  //   }).then(response => response.json()).then(data => {
-  //     console.log(data)
-  //   })
-  // }, [])
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const token = await getAccessTokenSilently()
+      const auth0_user_obj = {
+        user,
+        client_access_token: token 
+      }
+      console.log(auth0_user_obj)
+    }
+    getUserMetadata()
+  }, [user, getAccessTokenSilently])
 
   const render = (props: RouteProps) => {
     return (
